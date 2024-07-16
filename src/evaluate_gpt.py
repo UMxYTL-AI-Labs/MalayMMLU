d16215db2255d1f1bd33c75fa6f457eee3ba9ae9
Poh Soon Chang <psoonchang@gmail.com>
2024-07-16 15:16:24 +0800

    first commit

---
+++ src/evaluate_gpt.py
@@ -0,0 +1,75 @@
+import argparse
+import pandas as pd
+from openai import OpenAI
+
+# usage python evaluate_gpt.py --model gpt-3.5-turbo --api_key $API_KEY --shot 0
+def main(api_key, model,shot):
+    client = OpenAI(api_key=api_key)
+    inputs = []
+    outputs = []
+    outputs_options = []
+    key2id = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4}
+    
+    data = pd.read_json(f'../data/MalayMMLU_{shot}shot.json')
+    if shot == 0:
+        for row in data.iterrows():
+            ques = row['prompt']
+            p = f"Berikut adalah soalan aneka pilihan tentang {row['subject']}. Sila berikan jawapan sahaja.\n\n" + ques + "\nJawapan:" 
+            inputs.append(p)
+        
+            idx_label = key2id[row['key']]
+            outputs.append(idx_label)
+            outputs_options.append(row['options'])
+    else:
+        for idx, row in data.iterrows():
+            ques =  data.iloc[idx][f'full_question_{shot}shot']
+                
+            inputs.append(ques)
+
+            idx_label = key2id[row['key']]
+            outputs.append(idx_label)
+            outputs_options.append(row['options'])
+    
+
+    print(inputs[0])
+    batch_requests = [
+        {
+            "custom_id": str(data.iloc[i].id),
+            "method": "POST",
+            "url": "/v1/chat/completions",
+            "body": {
+                "model": model,
+                "messages": [{"role": "user", "content": inputs[i]}],
+                "max_tokens": 50
+            }
+        } for i in range(len(inputs))
+    ]
+    
+    batch_df = pd.DataFrame(batch_requests)
+    batch_df.to_json(f"MalayMMLU-{shot}shot_{model}_batch_requests.jsonl", lines=True, orient="records")
+    
+    batch_input_file = client.files.create(
+        file=open(f"MalayMMLU-{shot}shot_{model}_batch_requests.jsonl", "rb"),
+        purpose="batch"
+    )
+    
+    batch_input_file_id = batch_input_file.id
+    
+    client.batches.create(
+        input_file_id=batch_input_file_id,
+        endpoint="/v1/chat/completions",
+        completion_window="24h",
+        metadata={
+            "description": f"MalayMMLU-0shot_{model}"
+        }
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Benchmarking MalayMMLU on GPT")
+    parser.add_argument("--model",required=True, type=str, help="GPT models", default="gpt-3.5-turbo")
+    parser.add_argument("--api_key", required=True, help="OpenAI API Key")
+    parser.add_argument("--shot",type=int, default=0)
+    
+    args = parser.parse_args()
+    main(api_key=args.api_key,model=args.model, shot=args.shot)
---
+++ src/utils.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+
+
+def softmax(x):
+    z = x - max(x)
+    numerator = np.exp(z)
+    denominator = np.sum(numerator)
+    softmax = numerator/denominator
+    return softmax
+
+
+@torch.no_grad()
+def get_logprobs_causal(model, tokenizer, prompt, device):
+    """
+    Input: Question + options + Answer: one of the option
+    Obtain the probability of this sequence
+    """
+    inputs = tokenizer(prompt, return_tensors="pt")
+    if model.config.model_type == 'falcon':
+        inputs.pop("token_type_ids")
+        
+    input_ids, output_ids = inputs["input_ids"].to(device), inputs["input_ids"][:, 1:].to(device)
+    for k, v in inputs.items():
+        inputs[k] = v.to(device) #"cuda", non_blocking=True)
+    outputs = model(**inputs, labels=input_ids)
+
+    logits = outputs.logits.to(torch.double).to(device)
+    output_ids = output_ids.to(logits.get_device()) #in case of paralellism
+    logprobs = torch.gather(F.log_softmax(logits, dim=2), 2, output_ids.unsqueeze(2))
+    
+    return logprobs.mean()
+
+def predict_classification_causal(model, tokenizer, input_text, labels, device):
+    """
+    Get the full answer probability = probability(Question + options + Answer: one of the option)
+    for each option 
+    """
+    probs = [get_logprobs_causal(model, tokenizer, input_text+label, device) for label in labels]
+    return probs
+
+def predict_classification_causal_by_letter(model, tokenizer, input_text, labels, device):
+    """
+    Tokenize option alphabets, get their indices to access the probabilities
+    Input: Question + options 
+    Output: Predicted answer
+    """
+    choices = ['A', 'B', 'C', 'D', 'E'][:len(labels)]
+    choice_ids = [tokenizer.encode(choice)[-1] for choice in choices]
+    with torch.no_grad():
+        inputs = tokenizer(input_text, return_tensors="pt")
+        input_ids = inputs["input_ids"].to(device)
+        if model.config.model_type == 'falcon':
+            inputs.pop("token_type_ids")
+        for k, v in inputs.items():
+            inputs[k] = v.to(device) #"cuda", non_blocking=True)
+        outputs = model(**inputs, labels=input_ids)
+        last_token_logits = outputs.logits[:, -1, :]
+        choice_logits = last_token_logits[:, choice_ids].detach().cpu().numpy()
+        conf = softmax(choice_logits[0])
+        pred = {0: "A", 1: "B", 2: "C", 3: "D", 4: "E"}[np.argmax(choice_logits[0])]
+    return conf, pred
+
