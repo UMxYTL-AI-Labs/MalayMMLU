f7c0279946bbeef6d4fcb3a479c500fea3c1abf6
soonchangAI <scpoh.phd@gmail.com>
2024-09-25 09:59:35 +0800

    commit

--- src/calculate_accuracies.py
+++ src/calculate_accuracies.py
@@ -3,10 +3,24 @@ import os
 import json
 import argparse
 
-def calculate_accuracy(task,mmlu,filename,keep_idxs=None):
+def calculate_accuracy(task,mmlu,filename,closed_flag, keep_idxs=None):
     if task == "MalayMMLU":
         if keep_idxs == None:
-            if 'gpt' not in filename:
+            if closed_flag: #elif "gpt" in filename or "glm" in filename:
+                result = pd.read_json(filename, lines=True)
+                correct_first = 0
+                correct_full = 0
+                for i in range(len(result)):
+                    id = result.iloc[i].custom_id
+                    ground_truth = mmlu[mmlu.id == id].iloc[0]
+                    if result.iloc[i]['response']['body']['choices'][0]['message']['content'] != "":
+                        if result.iloc[i]['response']['body']['choices'][0]['message']['content'][0] == ground_truth.key: #mmlu.iloc[i].key:
+                            correct_first += 1
+                        if result.iloc[i]['response']['body']['choices'][0]['message']['content'] == ground_truth.answer: #mmlu.iloc[i].answer:
+                            correct_full += 1
+                return correct_first/len(mmlu)*100, correct_full/len(mmlu)*100
+            
+            else: #if 'gpt' not in filename:
                 
                 result = pd.read_csv(filename)
                 
@@ -30,18 +44,26 @@ def calculate_accuracy(task,mmlu,filename,keep_idxs=None):
                             correct_org += 1
     
                     return  correct_org/len(mmlu)*100
-            elif "gpt" in filename:
+
+        else:
+            if closed_flag: #elif "gpt" in filename:
+                print(filename)
                 result = pd.read_json(filename, lines=True)
                 correct_first = 0
                 correct_full = 0
                 for i in range(len(result)):
-                    if result.iloc[i]['response']['body']['choices'][0]['message']['content'][0] == mmlu.iloc[i].key:
-                        correct_first += 1
-                    if result.iloc[i]['response']['body']['choices'][0]['message']['content'] == mmlu.iloc[i].answer:
-                        correct_full += 1
-                return correct_first/len(mmlu)*100, correct_full/len(mmlu)*100
-        else:
-            if 'gpt' not in filename:
+                    id = result.iloc[i].custom_id
+          
+                    ground_truth = mmlu[mmlu.id == id].iloc[0]
+                    
+                    if result.iloc[i]['response']['body']['choices'][0]['message']['content'] != "":
+                        if result.iloc[i]['response']['body']['choices'][0]['message']['content'][0] == ground_truth.key  and result.iloc[i].name in keep_idxs: #mmlu.iloc[i].key:
+                            correct_first += 1
+                        if result.iloc[i]['response']['body']['choices'][0]['message']['content'] == ground_truth.answer and result.iloc[i].name in keep_idxs: #: #mmlu.iloc[i].answer:
+                            correct_full += 1
+                return correct_first/len(keep_idxs)*100, correct_full/len(keep_idxs)*100
+            
+            else: #if 'gpt' not in filename:
                 
                 result = pd.read_csv(filename)
                 
@@ -65,19 +87,9 @@ def calculate_accuracy(task,mmlu,filename,keep_idxs=None):
                             correct_org += 1
     
                     return  correct_org/len(keep_idxs)*100
-            elif "gpt" in filename:
-                print(filename)
-                result = pd.read_json(filename, lines=True)
-                correct_first = 0
-                correct_full = 0
-                for i in range(len(result)):
-                    if result.iloc[i]['response']['body']['choices'][0]['message']['content'][0] == mmlu.iloc[i].key and result.iloc[i].name in keep_idxs:
-                        correct_first += 1
-                    if result.iloc[i]['response']['body']['choices'][0]['message']['content'] == mmlu.iloc[i].answer and result.iloc[i].name in keep_idxs:
-                        correct_full += 1
-                return correct_first/len(keep_idxs)*100, correct_full/len(keep_idxs)*100
 
-def main(pred_files, shot, output_dir):
+
+def main(pred_files, shot, output_dir,closed_flag):
     os.makedirs(output_dir, exist_ok=True)
     mmlu = pd.read_json(f'data/MalayMMLU_{shot}shot.json')
     
@@ -93,9 +105,18 @@ def main(pred_files, shot, output_dir):
         
         for cat in mmlu.category.unique():
             keep_ids = list(mmlu[mmlu.category == cat].index)
-            
-            if "gpt" not in pred_file_str:
-                org_acc = calculate_accuracy("MalayMMLU", mmlu, pred_file_str, keep_ids)
+
+            if closed_flag: #"gpt" in pred_file_str:
+                first_acc, full_acc = calculate_accuracy("MalayMMLU", mmlu, pred_file_str, closed_flag, keep_ids)
+                split = pred_file_str.split("_")
+                models += [split[1], split[1]]
+                shots += [split[2].split(".")[0], split[2].split(".")[0]]
+                by_letter += ["True", "False"]
+                org_accs += [first_acc, full_acc]
+
+                categories += [cat] * 2            
+            else: #elif "gpt" not in pred_file_str:
+                org_acc = calculate_accuracy("MalayMMLU", mmlu, pred_file_str,  closed_flag, keep_ids)
                 org_accs.append(org_acc)
                 
                 split = pred_file_str.split("_")
@@ -104,55 +125,8 @@ def main(pred_files, shot, output_dir):
                 shots.append(split[4].split(".")[0])
                 categories += [cat]
             
-            elif "gpt" in pred_file_str:
-                first_acc, full_acc = calculate_accuracy("MalayMMLU", mmlu, pred_file_str, keep_ids)
-                split = pred_file_str.split("_")
-                models += [split[1], split[1]]
-                shots += [split[2].split(".")[0], split[2].split(".")[0]]
-                by_letter += ["True", "False"]
-                org_accs += [first_acc, full_acc]
-                categories += [cat] * 2
-        if "gpt" not in pred_file_str:
-            # Append results for the current prediction file
-            df = pd.DataFrame({
-                "Model": models,
-                "Accuracy": org_accs,
-                "shot": shots,
-                "by_letter": by_letter,
-                "category": categories
-            })
-            print(df)
-            category2amount = dict(mmlu.category.value_counts())
-            print(category2amount)
-            sum_acc = 0
 
-            for category in category2amount.keys():
-                category_df = df[df.category == category]
-                if not category_df.empty:
-                    sum_acc += category2amount[category] * category_df.iloc[0]['Accuracy'] 
-            average_acc = sum_acc / len(mmlu)
-
-            accuracy_info = {
-                'average accuracy': average_acc
-            }
-            for i in range(len(df)):
-                category = df.iloc[i].category
-                accuracy_info[f'accuracy for {category}'] = df.iloc[i].Accuracy
-
-            model_name = df['Model'].iloc[0]
-            exp = "first" if df['by_letter'].iloc[0] == "True" else "full"
-            shot = df['shot'].iloc[0]
-            print("Model :", model_name)
-            print("Metric :",exp)
-            print("Shot :",shot)
-            for k,v in accuracy_info.items():
-                print(k,v)
-
-            output_file = os.path.join(output_dir, f"accuracy_info_{model_name}_{shot}_{exp}.json")
-            
-            with open(output_file, "w") as f:
-                json.dump(accuracy_info, f, indent=4)
-        else:
+        if closed_flag: #else:
             for exp in ["first","full"]:
                 # Append results for the current prediction file
                 df = pd.DataFrame({
@@ -197,10 +171,53 @@ def main(pred_files, shot, output_dir):
                 
                 with open(output_file, "w") as f:
                     json.dump(accuracy_info, f, indent=4)
+
+        else:#if "gpt" not in pred_file_str:
+            # Append results for the current prediction file
+            df = pd.DataFrame({
+                "Model": models,
+                "Accuracy": org_accs,
+                "shot": shots,
+                "by_letter": by_letter,
+                "category": categories
+            })
+            print(df)
+            category2amount = dict(mmlu.category.value_counts())
+            print(category2amount)
+            sum_acc = 0
+
+            for category in category2amount.keys():
+                category_df = df[df.category == category]
+                if not category_df.empty:
+                    sum_acc += category2amount[category] * category_df.iloc[0]['Accuracy'] 
+            average_acc = sum_acc / len(mmlu)
+
+            accuracy_info = {
+                'average accuracy': average_acc
+            }
+            for i in range(len(df)):
+                category = df.iloc[i].category
+                accuracy_info[f'accuracy for {category}'] = df.iloc[i].Accuracy
+
+            model_name = df['Model'].iloc[0]
+            exp = "first" if df['by_letter'].iloc[0] == "True" else "full"
+            shot = df['shot'].iloc[0]
+            print("Model :", model_name)
+            print("Metric :",exp)
+            print("Shot :",shot)
+            for k,v in accuracy_info.items():
+                print(k,v)
+
+            output_file = os.path.join(output_dir, f"accuracy_info_{model_name}_{shot}_{exp}.json")
+            
+            with open(output_file, "w") as f:
+                json.dump(accuracy_info, f, indent=4)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Calculate model prediction accuracy.')
     parser.add_argument('--pred_files', nargs='+', type=str,  help='List of prediction files.')
-    parser.add_argument('--all', action='store_true', help="Calculate accuracy for all prediction files in a directory")
+    parser.add_argument('--all', action='store_true', help="Flag for calculate accuracy for all prediction files in a directory")
+    parser.add_argument('--closed', action='store_true', help="Flag for closed source model. The prediction file should have a suffix 'closed'")
     parser.add_argument('--pred_dir',type=str, help='Directory containing prediction files. Only provide when include --all flag')
     parser.add_argument('--shot', type=str, required=True, help='Provide the number of shots: 0,1,2 or 3')
     parser.add_argument('--output_dir', type=str, required=True, help='Directory to save the accuracy JSON file.')
@@ -209,7 +226,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if args.all:
         pred_files = [ args.pred_dir +"/" + x for x in  os.listdir(args.pred_dir)]
-        main(pred_files, args.shot, args.output_dir)
+        print(pred_files)
+        main(pred_files, args.shot, args.output_dir, args.closed)
+
     else:
-        main(args.pred_files, args.shot, args.output_dir)
+        main(args.pred_files, args.shot, args.output_dir, args.closed)
 
---
+++ src/evaluate_vlm.py
@@ -0,0 +1,129 @@
+import argparse
+import pandas as pd
+import os
+from transformers import LlamaForCausalLM, LlamaTokenizer, AutoTokenizer, AutoModelForCausalLM
+from tqdm import tqdm
+from numpy import argmax
+import torch
+from utils import predict_classification_causal as predict_classification
+from utils import predict_classification_causal_by_letter as predict_classification_by_letter
+
+device = "cuda"
+# usage: python evaluate.py  --by_letter --shot 0 --task=MalayMMLU --base_model=google/gemma-2b-it --output_folder=$HOME/MalayMMLU/output/  --token $TOKEN
+
+def prepare_data(playground,model_name, tokenizer,task):
+    if task=="MalayMMLU":
+        inputs = []
+        outputs = []
+        outputs_options = []
+        key2id = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4}
+        shot = 0
+        data = pd.read_json(f'data/MalayMMLU_{shot}shot.json')
+        if playground:
+            data = data.iloc[:10]
+        for idx, row in data.iterrows():
+            ques =  data.iloc[idx]['prompt']
+            if "llama" in model_name.lower():
+
+                p = f"Berikut adalah soalan aneka pilihan tentang {row['subject']}. Sila berikan jawapan sahaja.\n\n" + ques
+                chat = [{"text": p}]
+                chat = tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=True) + "\nJawapan:"
+
+            else:
+                p = f"Berikut adalah soalan aneka pilihan tentang {row['subject']}. Sila berikan jawapan sahaja.\n\n" + ques + "\nJawapan:" 
+                chat = [{"text": p}]
+                chat = tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=True)                   
+            inputs.append(chat)
+
+            idx_label = key2id[row['key']]
+            outputs.append(idx_label)
+            outputs_options.append(row['options'])
+        return inputs, outputs, outputs_options
+
+def prepare_data_few_shot(shot, model_name, tokenizer,task):
+    if task == "MalayMMLU":
+        inputs = []
+        outputs = []
+        outputs_options = []
+        key2id = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4}
+        data = pd.read_json(f'data/MalayMMLU_{shot}shot.json')
+
+        for i in range(len(data)):
+            row = data.iloc[i]
+                
+            if "llama" in model_name.lower():
+                p = data.iloc[i][f'full_question_{shot}shot_llama']
+                chat = [{"role": "user", "content": p}]
+                chat = tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=True) +"Jawapan:"
+            else:
+                p = data.iloc[i][f'full_question_{shot}shot']
+                chat = [{"role": "user", "content": p}]
+                chat = tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=True)
+            inputs.append(chat)
+
+            idx_label = key2id[row['key']]
+            outputs.append(idx_label)
+            outputs_options.append(row['options'])
+        return inputs, outputs, outputs_options
+            
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--by_letter", action='store_true')
+    parser.add_argument("--base_model", type=str, help="Path to pretrained model", required=True)
+    parser.add_argument("--output_folder", type=str, default="output", required=True)
+    parser.add_argument("--playground", type=bool,default=False)
+    parser.add_argument("--task",type=str, default="MalayMMLU")
+    parser.add_argument("--shot",type=int, default=0)
+    parser.add_argument("--token",type=str)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    os.makedirs(args.output_folder, exist_ok=True)
+        
+    tokenizer_class = LlamaTokenizer if ('llama' in args.base_model and ("Llama-3" not in args.base_model and "Llama-2" not in args.base_model)) else AutoTokenizer
+    model_class = LlamaForCausalLM if ('llama' in args.base_model and ("Llama-3" not in args.base_model and "Llama-2" not in args.base_model)) else AutoModelForCausalLM
+
+    SAVE_FILE = f'{args.output_folder}/{args.task}_result_{args.base_model.split("/")[-1]}_{args.by_letter}_{args.shot}shot.csv'
+
+    tokenizer = tokenizer_class.from_pretrained(args.base_model,token=args.token,trust_remote_code=True)
+    
+    model = model_class.from_pretrained(args.base_model, token=args.token, torch_dtype=torch.float16, trust_remote_code=True, device_map= "cuda")
+
+    model.eval()
+
+    print(args.task)
+    
+    playground = args.playground # Enable testing of code with only 10 examples of questions
+    if args.shot == 0:
+        inputs, golds, outputs_options = prepare_data(playground, args.base_model, tokenizer,args.task)
+        print(inputs[0])
+    else:
+        inputs, golds, outputs_options = prepare_data_few_shot(args.shot,args.base_model, tokenizer,args.task)
+        print(inputs[0])
+    preds = []
+    probs = []
+    for idx in tqdm(range(len(inputs))):
+        if not args.by_letter: # full answer probability
+            out = predict_classification(model, tokenizer, inputs[idx], outputs_options[idx], device)
+            prob = [o.cpu().detach().item() for o in out] 
+            pred = argmax(prob)
+            preds.append(pred)
+
+        else: # first token probability
+            conf, pred = predict_classification_by_letter(model, tokenizer, inputs[idx], outputs_options[idx], device)
+            preds.append(pred)
+
+    output_df = pd.DataFrame()
+    output_df['input'] = inputs
+    output_df['golds'] = golds
+    output_df['options'] = outputs_options
+    output_df['preds'] = preds
+    print(output_df.iloc[0])
+    output_df.to_csv(SAVE_FILE, index=False)
+
+if __name__ == "__main__":
+    main()
