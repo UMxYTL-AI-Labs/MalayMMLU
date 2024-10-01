d4cf14a143f0a289d31dbe3731348d9b109b8047
YTLAILabs <163222502+YTLAILabs@users.noreply.github.com>
2024-10-01 10:42:19 +0800

    Update slurm_example.sh

--- scripts/slurm_example.sh
+++ scripts/slurm_example.sh
@@ -15,75 +15,75 @@
 module load miniconda
 source activate mmlu
 
-TOKEN=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
+TOKEN=<YOUR_HF_TOKEN>
 SHOT=0
 
 cd $HOME/MalayMMLU
 
 # first token accuracy
-python src/evaluate.py  --by_letter --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --by_letter --shot $SHOT --task=MalayMMLU \
                     --base_model=meta-llama/Meta-Llama-3-8B-Instruct  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --by_letter --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --by_letter --shot $SHOT --task=MalayMMLU \
                     --base_model=meta-llama/Llama-2-13b-chat-hf  \
                     --output_folder=output/ --token $TOKEN
 
 
-python src/evaluate.py  --by_letter --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --by_letter --shot $SHOT --task=MalayMMLU \
                     --base_model=meta-llama/Llama-2-7b-chat-hf  \
                     --output_folder=output/ --token $TOKEN
 
 
-python src/evaluate.py  --by_letter --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --by_letter --shot $SHOT --task=MalayMMLU \
                     --base_model=mistralai/Mistral-7B-Instruct-v0.3  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --by_letter --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --by_letter --shot $SHOT --task=MalayMMLU \
                     --base_model=mistralai/Mistral-7B-Instruct-v0.2  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --by_letter --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --by_letter --shot $SHOT --task=MalayMMLU \
                     --base_model=sail/Sailor-7B-Chat \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --by_letter --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --by_letter --shot $SHOT --task=MalayMMLU \
                     --base_model=SeaLLM-7B-v2.5  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --by_letter --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --by_letter --shot $SHOT --task=MalayMMLU \
                     --base_model=microsoft/Phi-3-medium-4k-instruct  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --by_letter --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --by_letter --shot $SHOT --task=MalayMMLU \
                     --base_model=microsoft/Phi-3-mini-4k-instruct \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --by_letter --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --by_letter --shot $SHOT --task=MalayMMLU \
                     --base_model=Qwen/Qwen1.5-7B-Chat  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --by_letter --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --by_letter --shot $SHOT --task=MalayMMLU \
                     --base_model=Qwen/Qwen1.5-4B-Chat  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --by_letter --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --by_letter --shot $SHOT --task=MalayMMLU \
                     --base_model=Qwen/Qwen1.5-1.8B-Chat  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --by_letter --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --by_letter --shot $SHOT --task=MalayMMLU \
                     --base_model=google/gemma-7b-it  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --by_letter --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --by_letter --shot $SHOT --task=MalayMMLU \
                     --base_model=google/gemma-2b-it  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --by_letter --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --by_letter --shot $SHOT --task=MalayMMLU \
                     --base_model=Yellow-AI-NLP/komodo-7b-base  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --by_letter --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --by_letter --shot $SHOT --task=MalayMMLU \
                     --base_model=mesolitica/mallam-5b-20k-instructions-v2 \
                     --output_folder=output/ --token $TOKEN
 
@@ -93,69 +93,69 @@ python src/calculate_accuracies.py --all --pred_dir output/$SHOT/ \
 
 # full answer accuracy
 
-python src/evaluate.py  --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --shot $SHOT --task=MalayMMLU \
                     --base_model=meta-llama/Meta-Llama-3-8B-Instruct  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --shot $SHOT --task=MalayMMLU \
                     --base_model=meta-llama/Llama-2-13b-chat-hf  \
                     --output_folder=output/ --token $TOKEN
 
 
-python src/evaluate.py  --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --shot $SHOT --task=MalayMMLU \
                     --base_model=meta-llama/Llama-2-7b-chat-hf  \
                     --output_folder=output/ --token $TOKEN
 
 
-python src/evaluate.py  --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --shot $SHOT --task=MalayMMLU \
                     --base_model=mistralai/Mistral-7B-Instruct-v0.3  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --shot $SHOT --task=MalayMMLU \
                     --base_model=mistralai/Mistral-7B-Instruct-v0.2  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --shot $SHOT --task=MalayMMLU \
                     --base_model=sail/Sailor-7B-Chat \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --shot $SHOT --task=MalayMMLU \
                     --base_model=SeaLLM-7B-v2.5  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --shot $SHOT --task=MalayMMLU \
                     --base_model=microsoft/Phi-3-medium-4k-instruct  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --shot $SHOT --task=MalayMMLU \
                     --base_model=microsoft/Phi-3-mini-4k-instruct \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --shot $SHOT --task=MalayMMLU \
                     --base_model=Qwen/Qwen1.5-7B-Chat  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --shot $SHOT --task=MalayMMLU \
                     --base_model=Qwen/Qwen1.5-4B-Chat  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --shot $SHOT --task=MalayMMLU \
                     --base_model=Qwen/Qwen1.5-1.8B-Chat  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --shot $SHOT --task=MalayMMLU \
                     --base_model=google/gemma-7b-it  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --shot $SHOT --task=MalayMMLU \
                     --base_model=google/gemma-2b-it  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --shot $SHOT --task=MalayMMLU \
                     --base_model=Yellow-AI-NLP/komodo-7b-base  \
                     --output_folder=output/ --token $TOKEN
 
-python src/evaluate.py  --shot $SHOT --use_chat_template True  --task=MalayMMLU \
+python src/evaluate.py  --shot $SHOT --task=MalayMMLU \
                     --base_model=mesolitica/mallam-5b-20k-instructions-v2 \
                     --output_folder=output/ --token $TOKEN
 
